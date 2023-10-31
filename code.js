2b1bad4de636a7819347c09c45dfcba3b08c0eae
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-10-31 09:36:47 -0600

    Update code.js
    Haven't gotten any proper unit tests working so I just added two graphs to the code as test inputs.

--- code.js
+++ code.js
@@ -46,6 +46,56 @@ function dijkstra(graph, source) {
 	delete distGraph[source];
 	return distGraph;
 }
+
+/*
+     C--E
+    /   |
+A--B    |
+    \   |
+     D--F
+*/
+var graph0 = { 'A': {'B': 2},
+			   'B': {'A': 2, 'C': 1, 'D': 5},
+			   'C': {'B': 1, 'E': 1},
+			   'D': {'B': 5, 'F': 6},
+			   'E': {'C': 1, 'F': 9},
+			   'F': {'D': 6, 'E': 9} };
+
+/*
+   B----D
+  /|    |\
+ / |    | \ 
+A  |    |  F
+ \ |    | /
+  \|    |/
+   C----E
+*/
+var graph1 = { 'A': {'B': 2, 'C': 4},
+			   'B': {'A': 2, 'C': 1, 'D': 7},
+			   'C': {'A': 4, 'B': 1, 'E': 3},
+			   'D': {'B': 7, 'E': 2, 'F': 1},
+			   'E': {'C': 3, 'D': 2, 'F': 5},
+			   'F': {'E': 5, 'D': 1} };
+
+// test with graph0 and A as source node
+// correct output: { B: 2, C: 3, D: 7, E: 4, F: 13 }
+var distFromSource = [];
+var source = 'A';
+distFromSource = dijkstra(graph0, source);
+console.log(source + " distance from nodes: ");
+console.log(distFromSource);
+console.log("correct output: ")
+console.log("{ B: 2, C: 3, D: 7, E: 4, F: 13 }")
+
+// test with graph1 and A as source node
+// correct output: { B: 2, C: 4, D: 9, E: 7, F: 12 }
+distFromSource = [];
+distFromSource = dijkstra(graph1, source);
+console.log(source + " distance from nodes: ");
+console.log(distFromSource);
+console.log("correct output: ")
+console.log("{ B: 2, C: 4, D: 9, E: 7, F: 12 }")
+
 // how I'm implementing my adjacency list
 // I switched back to dictionaries, not sure what I was trying to accomlish using multidimensional arrays
 // var graph0 = { 'A': {'B': 2},
