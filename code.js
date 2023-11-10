7156dc369b7e4e4287797ebc2840c10e6cbf6e62
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-11-10 15:15:33 -0700

    Update code.js
    Finally think this implementation works. Passes all of the tests I've done and feel very good about it.

--- code.js
+++ code.js
@@ -6,9 +6,9 @@ function dijkstra(graph, source) {
 
 	// creates dictionary holding each node with distance of Infinity
 	// source node with distance of 0
-    	let unvisited = []
+    let unvisited = []
 	let distGraph = {}
-	for (var v in graph) {
+	for (let v in graph) {
 		if (v == source) 
 			distGraph[v] = 0;
 		else 
@@ -16,88 +16,29 @@ function dijkstra(graph, source) {
         unvisited.push(v)
 	}
     let currentNode = source
-    let stack = []
+    let neighbors = []
 
     // while unvisited nodes remain
     while(unvisited.length > 0) {
 
-        // select node with lowest distance
-        for (node in unvisited) {
-            if (distGraph[node] < distGraph[currentNode]) {
-                currentNode = node
-            }
-        }
-
         // for each edge connected to currentNode
-        for (neighbor in graph[currentNode]) {
-            stack.push(neighbor)
-            console.log("cur: " + currentNode)
-            console.log("stack: " + stack)
+        for (let neighbor in graph[currentNode]) {
+            neighbors.push(neighbor)
             let cost = distGraph[currentNode] + graph[currentNode][neighbor]
             distGraph[neighbor] = Math.min(distGraph[neighbor], cost)
         }
-        // remove currentNode from unvisited array
         let i = unvisited.indexOf(currentNode)
         unvisited.splice(i, 1)
         let lowest = Infinity
-        for(let i = 0; i < stack.length; i++) {
-            console.log("stack[i]: " + stack[i])
-            if (distGraph[stack[i]] < lowest)
-                currentNode = stack[i]
+        for(let i = 0; i < neighbors.length; i++) {
+            if( (distGraph[neighbors[i]] < lowest) && (unvisited.includes(neighbors[i])) ) {
+                currentNode = neighbors[i]
+				lowest = distGraph[neighbors[i]] 
+		}
         }
-        stack = []
     }
     return distGraph
 }
-
-/*
-     C--E
-    /   |
-A--B    |
-    \   |
-     D--F
-*/
-var graph0 = { 'A': {'B': 2},
-			   'B': {'A': 2, 'C': 1, 'D': 5},
-			   'C': {'B': 1, 'E': 1},
-			   'D': {'B': 5, 'F': 6},
-			   'E': {'C': 1, 'F': 9},
-			   'F': {'D': 6, 'E': 9} };
-
-/*
-   B----D
-  /|    |\
- / |    | \ 
-A  |    |  F
- \ |    | /
-  \|    |/
-   C----E
-*/
-var graph1 = { 'A': {'B': 2, 'C': 4},
-			   'B': {'A': 2, 'C': 1, 'D': 7},
-			   'C': {'A': 4, 'B': 1, 'E': 3},
-			   'D': {'B': 7, 'E': 2, 'F': 1},
-			   'E': {'C': 3, 'D': 2, 'F': 5},
-			   'F': {'E': 5, 'D': 1} };
-
-// test with graph0 and A as source node
-// correct output: { B: 2, C: 3, D: 7, E: 4, F: 13 }
-var distFromSource = [];
-var source = 'A';
-distFromSource = dijkstra(graph0, source);
-console.log(source + " distance from nodes: ");
-console.log(distFromSource);
-console.log("correct output: ")
-console.log("{ B: 2, C: 3, D: 7, E: 4, F: 13 }")
-
-// test with graph1 and A as source node
-// correct output: { B: 2, C: 4, D: 9, E: 7, F: 12 }
-distFromSource = [];
-distFromSource = dijkstra(graph1, source);
-console.log(source + " distance from nodes: ");
-console.log(distFromSource);
-console.log("correct output: ")
-console.log("{ B: 2, C: 4, D: 9, E: 7, F: 12 }")
 
 // how I'm implementing my adjacency list
 // I switched back to dictionaries, not sure what I was trying to accomlish using multidimensional arrays
